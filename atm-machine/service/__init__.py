53d153b0805dfa7d89ab05142e4c62c6862bfdb2
vsiddharthreddy <84514921+vsiddharthreddy@users.noreply.github.com>
2022-06-17 15:06:18 +0530

    Update __init__.py

--- atm-machine/service/__init__.py
+++ atm-machine/service/__init__.py
@@ -1,2 +1,38 @@
-def pin_check(pin):
-    if pin === account_details.pin
+class Service():
+    @staticmethod
+    """
+        this is checking input from users with database and hence called in service layer
+        """
+    def verify(name,email):
+        if(name === users.name and email === user_details.email):
+            return true
+
+    @staticmethod
+    """
+    this is checking input from users with database and hence called in service layer
+    function also calculates wheter user input for withdrawal amount <= balance and if true
+    then it will also print the new balance
+    """
+    def withdrawl():
+        if(accno === accounts.account_no and pin === accounts.pin):
+            if(withdrawalamt <= accounts.balance ):
+               curr_balance= accounts.balance - withdrawalamt
+               return True,curr_balance
+
+    @staticmethod
+
+    """
+    this is takign user input for deposit amount and adding to balance
+    """
+    def deposit():
+        curr_balance = accounts.balance
+        curr_balance += deposit_amt
+        return curr_balance
+
+    @staticmethod
+
+    """
+    checks the balance from database only after retrieving user name and emai
+    """
+    def balance_check():
+        return accounts.balance
