a626384ddc575c5b9f0e2f92dcf2fd6e712872f1
vsiddharthreddy <84514921+vsiddharthreddy@users.noreply.github.com>
2022-06-22 17:17:45 +0530

    Update atm_service.py

--- atm-machine/service/atm_service.py
+++ atm-machine/service/atm_service.py
@@ -1,19 +1,21 @@
 class Service():
     @staticmethod
-    """
-        this is checking input from users with database and hence called in service layer
-        """
+
     def verify(name,email):
+        """
+                this is checking input from users with database and hence called in service layer
+                """
         if(name == users.name and email == user_details.email):
             return true
     "june"
     @staticmethod
-    """
-    this is checking input from users with database and hence called in service layer
-    function also calculates wheter user input for withdrawal amount <= balance and if true
-    then it will also print the new balance
-    """
+
     def withdrawl(withdrawalamt,account_no,pin):
+        """
+            this is checking input from users with database and hence called in service layer
+            function also calculates wheter user input for withdrawal amount <= balance and if true
+            then it will also print the new balance
+            """
         if(accno === accounts.account_no and pin === accounts.pin):
             if(withdrawalamt <= accounts.balance ):
                curr_balance= accounts.balance - withdrawalamt
@@ -21,10 +23,11 @@ class Service():
 
     @staticmethod
 
-    """
-    this is takign user input for deposit amount and adding to balance
-    """
+
     def deposit(deposit_amt,account_no,pin):
+        """
+           this is takign user input for deposit amount and adding to balance
+           """
         if (accno === accounts.account_no and pin == = accounts.pin):
             curr_balance = accounts.balance
             curr_balance += deposit_amt
@@ -32,9 +35,10 @@ class Service():
 
     @staticmethod
 
-    """
-    checks the balance from database only after retrieving user name and emai
-    """
+
     def balance_check(account_no):
+        """
+            checks the balance from database only after retrieving user name and emai
+            """
         if(account_no == accounts.account_no):
             return accounts.balance
